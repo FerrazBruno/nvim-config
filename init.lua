621e811943753b45bec804551647ef227a96c4a6
FerrazBruno <brunohenriqueferraz94@gmail.com>
2025-07-23 15:41:31 -0300

    Alterando a cor do 'MatchParen'

--- init.lua
+++ init.lua
@@ -1058,3 +1058,10 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.api.nvim_set_hl(0, "MatchParen", {
+  fg = "#ff007c",
+  bg = "NONE",
+  bold = true,
+  underline = false
+})
