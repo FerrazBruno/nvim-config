cd95c29c524ecfe482e19e3814d1290845586b99
FerrazBruno <brunohenriqueferraz94@gmail.com>
2025-09-01 20:02:16 -0300

    Removendo arquivos do buffer do telescope.

--- init.lua
+++ init.lua
@@ -443,11 +443,12 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<C-d>'] = require('telescope.actions').delete_buffer },
+            n = { ['<C-d>'] = require('telescope.actions').delete_buffer },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
