4dfbdd8966e7a33cce9e0afda5dfa86c1faec746
Bruno Ferraz <brunohenriqueferraz94@gmail.com>
2026-01-14 20:27:23 -0300

    Adicionando catppuccin mocha

--- init.lua
+++ init.lua
@@ -959,40 +959,53 @@ require('lazy').setup({
   --        ]]
   --   end,
   -- },
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     ---@diagnostic disable-next-line: missing-fields
+  --     require('tokyonight').setup {
+  --       styles = {
+  --         comments = { italic = false }, -- Disable italics in comments
+  --         -- keywords = { italic = false }, -- defmacro fica em italico e isso resolve, não sei porque.
+  --       },
+  --     }
+  --
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --     -- Deixar o fundo transparente do neovim.
+  --     -- vim.cmd [[
+  --     --   highlight Normal guibg=NONE ctermbg=NONE
+  --     --   highlight NormalNC guibg=NONE ctermbg=NONE
+  --     --   highlight NormalFloat guibg=NONE ctermbg=NONE
+  --     --   highlight FloatBorder guibg=NONE ctermbg=NONE
+  --     --   highlight SignColumn guibg=NONE ctermbg=NONE
+  --     --   highlight VertSplit guibg=NONE ctermbg=NONE
+  --     -- ]]
+  --   end,
+  -- },
+  { "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function ()
+      require('catppuccin').setup({
+        flavor = 'mocha',
+        no_italic = false,
+      })
+      vim.cmd.colorscheme 'catppuccin'
+    end
+  },
   {
     "nvim-lualine/lualine.nvim",
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-  },
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-          -- keywords = { italic = false }, -- defmacro fica em italico e isso resolve, não sei porque.
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-      -- Deixar o fundo transparente do neovim.
-      -- vim.cmd [[
-      --   highlight Normal guibg=NONE ctermbg=NONE
-      --   highlight NormalNC guibg=NONE ctermbg=NONE
-      --   highlight NormalFloat guibg=NONE ctermbg=NONE
-      --   highlight FloatBorder guibg=NONE ctermbg=NONE
-      --   highlight SignColumn guibg=NONE ctermbg=NONE
-      --   highlight VertSplit guibg=NONE ctermbg=NONE
-      -- ]]
+      require("lualine").setup()
     end,
   },
 
